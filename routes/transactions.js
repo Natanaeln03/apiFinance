63eb077ecb59bfb1b399aa265b455c1d5245b4e4
Natanaeln03 <natancnascimento88@gmail.com>
2024-09-06 10:34:52 -0300

    modificado

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,20 +2,22 @@ const express = require('express'); //importar a framework express
 const router = express.Router(); //criar um roteador
 const transactionsController = require('../controllers/transactionsController'); //Importa o controlador de transações
 
+const authMiddleware = require('../middlewares/authMiddleware'); // Importa o middleware de autenticação 
+
 //definir uma rota para obter todas as transações
-router.get('/', transactionsController.getAllTransactions);
+router.get('/',  authMiddleware, transactionsController.getAllTransactions);
 
 //definindo uma rota para todas as transações
-router.post('/', transactionsController.addTransaction);
+router.post('/',  authMiddleware, transactionsController.addTransaction);
 
 //definindo uma rota para atualizar uma trasação existente(substituição completa)
-router.put('/:id', transactionsController.updateTransactionPut);
+router.put('/:id',  authMiddleware, transactionsController.updateTransactionPut);
 
 //Definindo uma rota para atualizar uma trasação existente(substituição parcial)
-router.patch('/:id', transactionsController.updateTransactionPatch);
+router.patch('/:id',  authMiddleware, transactionsController.updateTransactionPatch);
 
 //Definindo uma rota para atualizar uma trasação existente(substituição parcial)
-router.delete('/:id', transactionsController.deleteTransaction);
+router.delete('/:id',  authMiddleware, transactionsController.deleteTransaction);
 
 
 //exportando o roteador
